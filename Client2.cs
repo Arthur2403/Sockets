49d17a56199bd61c002b4c9e77f880e4e1a7f02c
Arthur2403 <148908156+Arthur2403@users.noreply.github.com>
2025-12-11 00:14:19 +0200

    Client2

---
+++ Client2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    public class TCPClient
+    {
+        public static void ConnectToServer()
+        {
+            Socket clientSocket = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp
+            );
+
+            try
+            {
+                IPEndPoint serverEndPoint = new IPEndPoint(
+                    IPAddress.Parse("127.0.0.1"),
+                    8080
+                );
+
+                clientSocket.Connect(serverEndPoint);
+                Console.WriteLine("Підключено до сервера! Ведіть ваш запрос(time or date)");
+
+                string message = Console.ReadLine();
+                byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+                clientSocket.Send(messageBytes);
+
+                byte[] buffer = new byte[1024];
+                int bytesReceived = clientSocket.Receive(buffer);
+                string response = Encoding.UTF8.GetString(buffer, 0, bytesReceived);
+                Console.WriteLine($"Отримано рядок: {response}");
+
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Помилка: {ex.Message}");
+            }
+        }
+    }
+}
